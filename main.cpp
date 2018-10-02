90ebf742c923279c786006839048cdfe47994ff5
reza-rezvani2052 <reza-rezvani2052@gmail.com>
2018-10-02 20:36:23 +0330

    versio barname ra ezafe kardam. name karbari sarasari git ra az HajReza be reza-rezvani2052 taghir dadam

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setLayoutDirection(Qt::RightToLeft);
+    a.setApplicationVersion("1.0.0");
 
     MainWindow w;
     w.show();
